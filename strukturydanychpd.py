e7403769a26ff84eed5a73f9b8907b7b9d5a2576
Wojciesfpw <01153020@pw.edu.pl>
2021-03-15 20:20:13 +0100

    gitgit

--- strukturydanychpd.py
+++ strukturydanychpd.py
@@ -6,13 +6,16 @@ uop = {"C":C, "O":O, "H":H}
 
 etanol = {"C":2, "H":6, "O":1}
 # etanol = {"C":2, "H":5, "O":1, "H":1} źle, wartość pod indeksem H zostaje nadpisana!
+
 print(etanol)
 print(uop["C"]["z"],uop["O"]["masa"]) #result: 6 16.0
-
+print(uop)
 
 #Oblicz masę molową etanolu oraz oblicz ile elektronów ma w sumie ta cząsteczka.
 #W tym celu napisz pętlę biegnącą po odpowiednich kluczach słownika etanol i sumującą oddzielnie liczby masowe oraz liczby atomowe.
 #Oczekiwane wyniki: suma_m = 46.068, suma_z = 26
 
-# oblicz sumaryczną masę molową substancji sumując wartości "masa"
-# oblicz sumaryczną liczbę elektronów w jednej cząsteczne sumując wartości "z"
+suma_m = etanol["C"] * uop ["C"]['masa'] + etanol["H"] * uop ["H"]['masa'] + etanol["O"] * uop ["O"]['masa']
+print(suma_m)
+suma_z = etanol["C"] * uop ["C"]['z'] + etanol["H"] * uop ["H"]['z'] + etanol["O"] * uop ["O"]['z']
+print(suma_z)
